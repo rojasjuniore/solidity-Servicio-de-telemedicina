fd79084b00888ae4e9442e3de8f5732fe8381d6f
junior rojas <rojasjuniore@gmail.com>
2021-09-18 13:39:27 -0500

    agregando

---
+++ OperacionesBasicas.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.4 <0.7.0;
+pragma experimental ABIEncoderV2;
+import "./SafeMath.sol";
+
+contract OperacionesBAsicas {
+    using SafeMath for uint256;
+
+    // contractato bstracto
+    constructor() internal {}
+
+    // calcular el precio del token
+    function PrecioToken(uint256 _numTokens) internal pure returns (uint256) {
+        // conversion de tokens a ethers: 1 Token -> 1e18 Ethers
+        return _numTokens * (1 ether);
+    }
+
+    // obtener el banlance de la cuenta
+    function getBalance(address _address) public view returns (uint256) {
+        return payable(address(this)).balance;
+    }
+
+    // funcion para pasar  de uint a string
+    function uint2str(uint256 _i)
+        internal
+        pure
+        returns (string memory _uintAsString)
+    {
+        if (_i == 0) {
+            return "0";
+        }
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint256 k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = bytes1(uint8(48 + (_i % 10)));
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+}
---
+++ fabrica.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.4 <0.7.0;
+pragma experimental ABIEncoderV2;
+import "./SafeMath.sol";
+import "./ERC20.sol";
+import "./OperacionesBasicas.sol";
+
+contract FabricaDeSeguros is ERC20, OperacionesBasicas {}
